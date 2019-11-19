36cef6b956a6dd78b97c9004698c5db8847a2568
JuanjoHx <jmelendez@jjcorp.com.mx>
2019-11-19 09:28:50 -0600

    1 file modified, 5 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { CartComponent } from './cart/cart.component';
+import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   imports: [
@@ -20,7 +22,9 @@ import { CartComponent } from './cart/cart.component';
     AppComponent,
     TopBarComponent,
     ProductListComponent,
-    CartComponent
+    CartComponent,
+    ProductAlertsComponent,
+    ProductDetailsComponent
   ],
   bootstrap: [ AppComponent ]
 })
